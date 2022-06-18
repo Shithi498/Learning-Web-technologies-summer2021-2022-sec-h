06d241017f41b4665efbdfef5fcb3431dd3ac595
Shithi498 <101502082+Shithi498@users.noreply.github.com>
2022-06-18 09:15:33 +0600

    Add files via upload

--- Lab3_task2.php
+++ Lab3_task2.php
@@ -1,8 +1,15 @@
-<?php
-session_start();
-$email = $_REQUEST['email'];
-if($name != null)
-{
-	echo "invalid email"
+<?php 
+$email=$_REQEST['mail'];
+if($email!=null){
+	if(filter var($mail,FILTER_VALIDATE_EMAIL)){
+	   echo "successful";
+	}
+	else{
+	echo "Invalid request";
+	}
+
+}
+else{
+	echo "Invalid request";
 }
 ?>
